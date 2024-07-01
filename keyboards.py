3fcd4f7618259151e49e044e031e1b604014d8ea
Komori <121684959+Komorif@users.noreply.github.com>
2024-07-01 13:23:11 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -6,14 +6,12 @@ import os
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton, callback_query
 
 
-# Клавиатура
+# Клавиатуры
 mainMenu_mineShield = InlineKeyboardMarkup(row_width=2)
 mainMenu_in_registration = InlineKeyboardButton(text="Заполнить анкету", callback_data="mainMenu_in_registration")
 
 mainMenu_mineShield.add(mainMenu_in_registration)
 
-
-# Кнопка назад
 like_button = InlineKeyboardMarkup(row_width=2)
 like = InlineKeyboardButton(text="Нравится💘", callback_data="like")
 
