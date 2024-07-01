acd8ea713575535b49d1708b748a8b146ed16cde
Komori <121684959+Komorif@users.noreply.github.com>
2024-07-01 03:23:07 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,32 +1,60 @@
 import sqlite3 as sq
-
-db = sq.connect('tg.db')
-cur = db.cursor()
+from aiogram.dispatcher import FSMContext
 
 
+# Создание таблицы
 async def db_start():
-    cur.execute("CREATE TABLE IF NOT EXISTS accounts("
-                "id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                "tg_id INTEGER)")
-    cur.execute("CREATE TABLE IF NOT EXISTS items("
-                "i_id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                "name TEXT,"
-                "years INTEGER, "
-                "blogger TEXT, "
-                "hobbies TEXT, "
-                "city TEXT)")
+    global db, cur
+    db = sq.connect('tg.db')
+    cur = db.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY,
+        username TEXT,
+        name TEXT,
+        years INTEGER,
+        blogger TEXT,
+        hobbies TEXT,
+        city TEXT
+    )''')
     db.commit()
 
 
-async def cmd_start_db(user_id):
-    user = cur.execute("SELECT * FROM accounts WHERE tg_id == {key}".format(key=user_id)).fetchone()
-    if not user:
-        cur.execute("INSERT INTO accounts (tg_id) VALUES ({key})".format(key=user_id))
-        db.commit()
-
-
-async def add_item(state):
+# задаем "ключи" для каждого столбца
+async def add_item(state: FSMContext):
     async with state.proxy() as data:
-        cur.execute("INSERT INTO items (name, years, blogger, hobbies, city) VALUES (?, ?, ?, ?, ?)",
-                    (data['name'], data['years'], data['blogger'], data['hobbies'], data['city']))
+        cur.execute("INSERT INTO users (user_id, username, name, years, blogger, hobbies, city) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                       (data['user_id'], data['username'], data['name'], data['years'], data['blogger'], data['hobbies'], data['city']))
         db.commit()
+
+
+"""
+Сортировку по похожести
+1) возраст (разница должна быть не более 2 лет)
+2) город (не важно написано с caps lock или нет)
+"""
+
+async def get_similar_users(user_id, years_range=2):
+    cur.execute("SELECT * FROM users WHERE user_id = ?", (user_id,))
+    user = cur.fetchone()
+    if not user:
+        return None
+
+    user_years = user[3]
+    user_city = user[6].lower()
+
+    cur.execute("SELECT * FROM users WHERE user_id != ?", (user_id,))
+    all_users = cur.fetchall()
+
+    # Подбор пользователей с учетом критерия возраста и города
+    similar_users = []
+    for u in all_users:
+        if abs(u[3] - user_years) <= years_range or u[6].lower() == user_city:
+            similar_users.append(u)
+
+    return similar_users
+
+
+# Для проверки на уникальность user_id
+async def user_exists(user_id: int):
+    cur.execute("SELECT * FROM users WHERE user_id = ?", (user_id,))
+    return cur.fetchone()
